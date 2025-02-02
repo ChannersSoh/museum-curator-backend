a1d93d84600868f6665fb23201cc28f58a10a872
Charnjeet Sohal <charnjeetsohal@outlook.com>
2025-02-02 22:48:58 +0000

    attempt to fix ports

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import app from "./app";
 import { createServer, Server } from "http";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || "5000";
 
-export function startServer(port: number = Number(PORT)): Server {
+export function startServer(port: number = parseInt(PORT, 10)): Server {
   const server = createServer(app);
 
   server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log("Server is running on PORT: ${port}");
+  });
+
+  server.on("error", (err) => {
+    console.error("Server Error:", err);
   });
 
   return server;
