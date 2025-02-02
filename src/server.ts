5238384b2c218b6446f42fc65ea7415010aed54b
Charnjeet Sohal <charnjeetsohal@outlook.com>
2025-02-02 22:13:42 +0000

    fixed dynamic ports

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import app from "./app";
 import { Server } from "http";
 
-export function startServer(port: number = 5000): Server {
+export function startServer(port: number = parseInt(process.env.PORT || "5000", 10)): Server {
   const server = app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
+
   return server;
 }
 
